f2a65442d31fff274afebed11784a93fc3aa467b
ArvindAspire <arvind6tomar@gmail.com>
2022-08-28 20:38:20 +0530

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Test {
+
+    public static void main(String args[]){
+        String password  ="AUzs-nV";
+        if(!password.matches(".*[0-9].*"))
+            System.out.println("Yes");
+
+
+        if(!password.matches("(.*[a-z].*)")){
+            System.out.println("capital");
+        }
+        if(!password.matches(".*[0-9].*")){
+            System.out.println("number");
+        }
+        if(!password.matches(".*[!@#$%^&*()-+].*")){
+            System.out.println("special");
+        }
+        if(!password.matches(".*[-].*")){
+            System.out.println("special2");
+        }
+
+    }
+
 }
